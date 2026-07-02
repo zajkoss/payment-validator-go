d69ce262392fdc3ca84b8eb0ae18b54f8231869d
zajkoss <lukasz100.96@gmail.com>
2026-07-02 21:14:15 +0200

    payment validator with worker pool

--- cmd/validator/main.go
+++ cmd/validator/main.go
@@ -2,8 +2,69 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"payment-validator-go/internal/validator"
+	"strings"
 )
 
 func main() {
-	fmt.Println("validator start")
+	fmt.Println("=== Validator start ===")
+	if len(os.Args) < 2 {
+		fmt.Println("Required argument missing - directory")
+		os.Exit(1)
+	}
+
+	directory := os.Args[1]
+	fmt.Printf("\tDirectory: %s\n", directory)
+
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		fmt.Printf("Error reading directory: %s\n", err)
+		os.Exit(1)
+	}
+
+	//Standard solution, iterations
+	/*
+		for _, file := range files {
+			if strings.HasSuffix(file.Name(), ".xml") {
+				fmt.Printf("\t\tValidation of: %s\n", file.Name())
+				result, err := validator.ValidateFile(directory + "/" + file.Name()) //Required to change validateFile to public
+				if err != nil {
+					fmt.Printf("\t\t\tError: %s\n", err)
+					continue
+				}
+				fmt.Printf("\t\t\tFile: %s, Valid: %v, Errors: %v\n", result.Filename, result.IsValid, result.Error)
+
+			}
+		}
+	*/
+	var paths []string
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".xml") {
+			paths = append(paths, directory+"/"+file.Name())
+		}
+	}
+	results := validator.RunWorkerPool(paths, 3)
+
+	totalFiles := len(paths)
+	totalValid := 0
+	totalInvalid := 0
+
+	fmt.Println("=== Validation Report ===")
+	for _, result := range results {
+		status := "OK"
+		if result.IsValid {
+			totalValid++
+			fmt.Printf("\t%s - %s\n", result.Filename, status)
+		} else {
+			totalInvalid++
+			status = "FAIL"
+			fmt.Printf("\t%s - %s\n", result.Filename, status)
+			for _, e := range result.Error {
+				fmt.Printf("\t\t- %s\n", e)
+			}
+		}
+	}
+	fmt.Printf("Total files: %d Total valid: %d, Total invalid: %d\n", totalFiles, totalValid, totalInvalid)
+
 }
--- internal/validator/worker.go
+++ internal/validator/worker.go
@@ -1,3 +1,148 @@
-package main
+package validator
 
-func main() {}
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var validCountries = map[string]bool{
+	"PL": true,
+	"DE": true,
+	"GB": true,
+	"NL": true,
+}
+
+type FileStatus struct {
+	Filename string
+	IsValid  bool
+	Error    []string
+}
+
+type Document struct {
+	XMLName          xml.Name         `xml:"Document"`
+	CstmrCdtTrfInitn CstmrCdtTrfInitn `xml:"CstmrCdtTrfInitn"`
+}
+
+type CstmrCdtTrfInitn struct {
+	PmtInf PmtInf `xml:"PmtInf"`
+}
+
+type PmtInf struct {
+	CdtTrfTxInf []CdtTrfTxInf `xml:"CdtTrfTxInf"`
+}
+
+type CdtTrfTxInf struct {
+	Amt      Amt      `xml:"Amt"`
+	CdtrAcct CdtrAcct `xml:"CdtrAcct"`
+}
+
+type Amt struct {
+	InstdAmt InstdAmt `xml:"InstdAmt"`
+}
+
+type InstdAmt struct {
+	Ccy   string `xml:"Ccy,attr"`  // atrybut XML: <InstdAmt Ccy="PLN">
+	Value string `xml:",chardata"` // wartość: 100.00
+}
+
+type CdtrAcct struct {
+	Id Id `xml:"Id"`
+}
+
+type Id struct {
+	IBAN string `xml:"IBAN"`
+}
+
+// Private method
+func validateFile(path string) (FileStatus, error) {
+	data, err := os.ReadFile(path)
+
+	if err != nil {
+		fmt.Printf("\t\t\t\tError reading file: %s\n", err)
+		return FileStatus{}, err
+	}
+
+	var doc Document
+	err = xml.Unmarshal(data, &doc)
+	if err != nil {
+		fmt.Printf("\t\t\t\tError while deserializing file: %s\n", err)
+		return FileStatus{}, err
+	}
+
+	result := FileStatus{Filename: path}
+	for _, tx := range doc.CstmrCdtTrfInitn.PmtInf.CdtTrfTxInf {
+		iban := tx.CdtrAcct.Id.IBAN
+		currency := tx.Amt.InstdAmt.Ccy
+		amount := tx.Amt.InstdAmt.Value
+
+		if iban == "" {
+			result.Error = append(result.Error, "IBAN cannot be blank")
+		} else if len(iban) != 28 {
+			result.Error = append(result.Error, "IBAN must be 28 characters")
+		} else if !validCountries[iban[:2]] {
+			result.Error = append(result.Error, "IBAN has to start with a letters")
+		}
+
+		value, err := strconv.ParseFloat(amount, 64)
+		if err != nil {
+			result.Error = append(result.Error, err.Error())
+		}
+
+		if value <= 0 {
+			result.Error = append(result.Error, "Amount must be positive")
+		}
+
+		if currency == "" {
+			result.Error = append(result.Error, "Currency cannot be blank")
+		} else if len(currency) != 3 {
+			result.Error = append(result.Error, "Currency must be 3 characters")
+		}
+	}
+	result.IsValid = len(result.Error) == 0
+	return result, nil
+}
+
+// Public method
+// Worker pool.
+// Arg: paths - files to process, workers - jobs to run the same time
+func RunWorkerPool(paths []string, workers int) []FileStatus {
+	//input channel, string as input, and buffor allows to decide how many files shoule be loaded to memory
+	jobs := make(chan string, len(paths))
+	//output channel, how many result we can keep in memory before receive
+	results := make(chan FileStatus, len(paths))
+
+	//Start workers async
+	for w := 1; w <= workers; w++ {
+		fmt.Printf("WP: Starting worker %d\n", w)
+		go func() {
+			for path := range jobs {
+				result, err := validateFile(path)
+				//if err == nil {
+				//	results <- result
+				//}
+				if err != nil {
+					results <- FileStatus{Filename: path, IsValid: false, Error: []string{err.Error()}}
+				} else {
+					results <- result
+				}
+			}
+		}()
+	}
+
+	fmt.Printf("WP: Loading data to process...\n")
+	for _, path := range paths {
+		jobs <- path
+	}
+
+	fmt.Printf("WP: No more files to process, closing jobs channel...\n")
+	close(jobs)
+
+	fmt.Printf("WP: Grab all result in one array...\n")
+	var allResults []FileStatus
+	for range paths {
+		allResults = append(allResults, <-results)
+	}
+	return allResults
+}
